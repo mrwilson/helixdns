96385ac14279d1a08ffa24f3e8d761f8182dcaaf
Alex Wilson <a.wilson@alumni.warwick.ac.uk>
2014-03-04 22:02:54 +0000

    Refactor server to be testable

--- server.go
+++ server.go
@@ -14,18 +14,14 @@ type HelixServer struct {
 }
 
 func Server(port int, etcdurl string) *HelixServer {
-  return &HelixServer {
-    Port: port,
-    Client: NewEtcdClient(etcdurl),
-  }
+  return &HelixServer{ Port: port, Client: NewEtcdClient(etcdurl) }
 }
 
 func (s HelixServer) Start() {
-  handler := newHandler(s.Client)
   server := &dns.Server{
     Addr:         ":"+strconv.Itoa(s.Port),
     Net:          "udp",
-    Handler:      dns.HandlerFunc(handler),
+    Handler:      dns.HandlerFunc(s.Handler),
     ReadTimeout:  10,
     WriteTimeout: 10,
   }
@@ -35,7 +31,7 @@ func (s HelixServer) Start() {
   server.ListenAndServe()
 }
 
-func getResponse(client Client, q dns.Question) (Response, error) {
+func (s HelixServer) getResponse(q dns.Question) (Response, error) {
   addr := dns.SplitDomainName(q.Name)
   path := []string{"helix"}
 
@@ -45,38 +41,36 @@ func getResponse(client Client, q dns.Question) (Response, error) {
 
   path = append(path, dns.TypeToString[q.Qtype])
 
-  return client.Get(strings.Join(path, "/"))
+  return s.Client.Get(strings.Join(path, "/"))
 }
 
-func newHandler(client Client) func(dns.ResponseWriter, *dns.Msg) {
-  return func (w dns.ResponseWriter, req *dns.Msg) {
-    m := new(dns.Msg)
-    m.SetReply(req)
+func (s HelixServer) Handler(w dns.ResponseWriter, req *dns.Msg) {
+  m := new(dns.Msg)
+  m.SetReply(req)
 
-    qType  := req.Question[0].Qtype
-    qClass := req.Question[0].Qclass
+  qType  := req.Question[0].Qtype
+  qClass := req.Question[0].Qclass
 
-    header := dns.RR_Header{Name: m.Question[0].Name, Rrtype: qType, Class: qClass, Ttl: 5}
+  header := dns.RR_Header{Name: m.Question[0].Name, Rrtype: qType, Class: qClass, Ttl: 5}
 
-    resp, err := getResponse(client, req.Question[0])
+  resp, err := s.getResponse(req.Question[0])
 
-    if err != nil {
-      log.Printf("Could not get record for %s", req.Question[0].Name)
-      w.WriteMsg(m)
-      return
-    }
-
-    switch qType {
-      case dns.TypeA:
-        m.Answer = make([]dns.RR, 1)
-        m.Answer[0] = &dns.A {Hdr: header, A: net.ParseIP(resp.Value())}
-      case dns.TypeAAAA:
-        m.Answer = make([]dns.RR, 1)
-        m.Answer[0] = &dns.AAAA {Hdr: header, AAAA: net.ParseIP(resp.Value())}
-      default:
-        log.Printf("Unrecognised record type: %d",qType)
-    }
-
+  if err != nil {
+    log.Printf("Could not get record for %s", req.Question[0].Name)
     w.WriteMsg(m)
+    return
   }
+
+  switch qType {
+    case dns.TypeA:
+      m.Answer = make([]dns.RR, 1)
+      m.Answer[0] = &dns.A {Hdr: header, A: net.ParseIP(resp.Value())}
+    case dns.TypeAAAA:
+      m.Answer = make([]dns.RR, 1)
+      m.Answer[0] = &dns.AAAA {Hdr: header, AAAA: net.ParseIP(resp.Value())}
+    default:
+      log.Printf("Unrecognised record type: %d",qType)
+  }
+
+  w.WriteMsg(m)
 }
