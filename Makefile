eb94204054ef1656f8be6b27ee1929c9aa813b11
Alex Wilson <a.wilson@alumni.warwick.ac.uk>
2014-03-09 21:34:23 +0000

    Integration tests

--- Makefile
+++ Makefile
@@ -4,6 +4,10 @@ install: deps
 test: deps
 	@go test -v
 
+integration-test: deps
+	@sh ./scripts/download_and_run_etcd.sh
+	@sh ./integration_tests.sh
+
 deps:
 	@go get github.com/coreos/go-etcd/etcd
 	@go get github.com/miekg/dns
---
+++ integration_tests.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+ETCD_URL=http://localhost:4001/v2/keys
+
+SetEtcdRecord(){
+  RECORD=$1
+  VALUE=$2
+  curl --silent -o /dev/null -XPUT ${ETCD_URL}/${RECORD} -d value="${VALUE}"
+}
+
+AssertRecordEquals(){
+  ADDRESS=$1
+  TYPE=$2
+  RECORD=$3
+
+  OUTPUT=$(dig ${ADDRESS} @localhost -p 9000 ${TYPE} +short)
+
+  if [ "${RECORD}" = "${OUTPUT}" ]; then
+    echo Successfully resolved ${ADDRESS} to ${RECORD}
+  else
+    echo Did not successfully resolve ${ADDRESS} to ${RECORD}: got ${OUTPUT} instead
+    exit 1
+  fi
+}
+
+go run cmd/hdns/hdns.go &
+SERVER_PID=$!
+sleep 5
+
+echo
+
+echo TESTING A RECORD...
+SetEtcdRecord      "helix/com/example/A" "123.123.123.123"
+AssertRecordEquals "example.com."    "A" "123.123.123.123"
+
+kill -9 ${SERVER_PID}
